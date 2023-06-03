6e083ff688aa000e270b203692b45bbb8eb75270
Ian Bicking <ianb@colorstudy.com>
2023-06-02 23:30:29 -0500

    Add a (unused) omit parameter. And fix parsing multiple attributes

--- src/parsetags.js
+++ src/parsetags.js
@@ -33,32 +33,46 @@ function trimClosing(s) {
 }
 
 function parseAttrs(s) {
-  const [tag, attrsText] = s.split(/\s+/, 2);
+  const tagMatch = s.match(/^([^\s]+)\s+(.*)$/);
+  if (!tagMatch) {
+    return [s, {}];
+  }
+  const tag = tagMatch[1];
+  const attrsText = tagMatch[2];
   const attrs = {};
   if (attrsText) {
-    Array.from(attrsText.matchAll(/([^=]+)="([^"]*)"/g)).forEach((match) => {
+    Array.from(attrsText.matchAll(/([^=\s]+)="([^"]*)"/g)).forEach((match) => {
       attrs[match[1].trim()] = match[2];
     });
   }
   return [tag, attrs];
 }
 
-export function serializeTags(tags) {
+export function serializeTags(tags, omit = null) {
   const lines = [];
   for (const tag of tags) {
     const attrs = Object.assign({}, tag);
     delete attrs.type;
     delete attrs.content;
-    lines.push(`<${tag.type}${serializeAttrs(attrs)}>`);
+    lines.push(`<${tag.type}${serializeAttrs(attrs, omit)}>`);
     lines.push(tag.content || "");
     lines.push(`</${tag.type}>\n`);
   }
   return lines.join("\n");
 }
 
-function serializeAttrs(attrs) {
+function serializeAttrs(attrs, omit = null) {
   const result = [];
   for (const [key, value] of Object.entries(attrs)) {
+    if (omit && Array.isArray(omit) && omit.includes(key)) {
+      continue;
+    }
+    if (omit && typeof omit === "function" && omit(key, value)) {
+      continue;
+    }
+    if (omit && typeof omit === "string" && key === omit) {
+      continue;
+    }
     result.push(` ${key}="${value}"`);
   }
   return result.join("");
