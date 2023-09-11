087170fbffccfa7d36c1af29f3483c82b11444bf
Ian Bicking <ianb@colorstudy.com>
2023-09-11 12:16:49 -0500

    Add watchSignals()

--- src/components/callback.js
+++ src/components/callback.js
@@ -1,21 +1,39 @@
 import { createContext } from "preact";
+import { effect } from "@preact/signals";
 
 export const ReturnResult = createContext((value) => { throw new Error("No provider") });
 
-export function getResult(containerSignal, component) {
+export function getResult(
+  containerSignal,
+  component,
+  { eventReceivers } = {}
+) {
   return new Promise((resolve, reject) => {
     function onDone(value) {
       containerSignal.value = <div class="relative">
         <div class="absolute inset-0 bg-white opacity-50 z-10"></div>
         {component}
       </div>;
-      // containerSignal.value = null;
       resolve(value);
+    }
+    if (eventReceivers) {
+      if (!Array.isArray(eventReceivers)) {
+        eventReceivers = [eventReceivers];
+      }
+      for (const receiver of eventReceivers) {
+        receiver.onDone = onDone;
+      }
     }
     containerSignal.value = <ReturnResult.Provider value={onDone}>
       {component}
     </ReturnResult.Provider>;
   });
+}
+
+export class EventReceiver {
+  constructor() {
+    this.onDone = null;
+  }
 }
 
 export async function fillSignal({ container, signal, request, mutator }) {
@@ -28,4 +46,24 @@ export async function fillSignal({ container, signal, request, mutator }) {
   }
   signal.value = value;
   return signal.value;
+}
+
+export async function watchSignals(signalMap) {
+  return new Promise((resolve) => {
+    let callCount = 0;
+    const destroyer = effect(() => {
+      if (callCount) {
+        // It's been called once
+        // FIXME: this should indicate what changed
+        // Maybe with {signalName: {oldValue, newValue}}
+        destroyer();
+        resolve();
+        return;
+      }
+      for (const [name, signal] of Object.entries(signalMap)) {
+        let _ = signal.value;
+      }
+      callCount++;
+    });
+  });
 }
